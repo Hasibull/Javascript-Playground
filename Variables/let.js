0b701f5e4f2155ae85a449421e9d553b1a7d127d
Hasibul <hasibulislam4500@gmail.com>
2020-10-12 15:06:57 +0600

    Declaring variables with let keyword

---
+++ Variables/let.js
@@ -0,0 +1,11 @@
+let number = 3;
+
+if(number === 3) {
+    let newNumber = 5;
+
+    console.log(newNumber + number); //it's work fine. :)
+}
+
+// console.log(newNumber); ---> gives error.
+// variables define with let keywor
+// has brace scope. in any brace block.
