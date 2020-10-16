4308d063ff239abc859933cb4c2d615f80ae929b
Hasibul <hasibulislam4500@gmail.com>
2020-10-16 17:34:30 +0600

    learning function declaration

---
+++ Functions/declaringFunction.js
@@ -0,0 +1,17 @@
+// usual declaration.
+
+function sum(value1, value2) {
+    return value1 + value2;
+}
+
+/// function as a variable.
+
+const substraction = function(value1, value2) {
+    return value1 - value2;
+}
+
+//invoking function.
+
+console.log(sum(2, 4)); /// 6
+console.log(substraction(5, 2)); /// 3
+
