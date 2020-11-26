2290f2ad324a6c6de1ec50073cc7ca4c0902b2e0
Hasibul <hasibulislam4500@gmail.com>
2020-11-26 12:25:59 +0600

    Standard input in JavaScript

---
+++ STD input output/input.js
@@ -0,0 +1,9 @@
+/*
+It depends on the environment that your JavaScript is executing in.
+--- In browser most input comes from HTML elements.
+--- NodeJs also has it's default input system.
+*/
+
+//take input from popUp menu.
+var name = prompt("Enter your name?"); //Entered value stored in name variable.
+
