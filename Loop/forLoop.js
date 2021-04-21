b2f11d7c8d2ff876267d3e17ddca5816b90e1c9f
Hasibul <hasibulislam4500@gmail.com>
2021-04-22 00:10:42 +0600

    Changing content in forloop file

--- Loop/forLoop.js
+++ Loop/forLoop.js
@@ -1,3 +1,37 @@
+
+/**
+ * Javascript mainly provide three kind of loop-
+ *      1.for loop.
+ *      2.while loop.
+ *      3.do-while loop.
+ * 
+ * here we practice for loop;
+ * 
+ *    There are three step common in every loop
+ *           i. Initialization.
+ *          ii. Condition.
+ *         iii. Increment/decrement the value of the loop variable.
+ */
+
+
 for(let i=0; i<5; i++){
-    console.log("Hello");
+    console.log("Hello!");
 }
+
+/**
+ * we use loop for repeated task which is same.
+ */
+
+/**
+ * we can use the below code to sum at a range.
+ */
+
+let start = 1; /// lower bound
+let end = 10; /// upper bound
+let sum = 0;  ///containing the sum.
+
+for(let i=start; i<=end; i++){
+    sum += i;
+}
+
+console.log("The sum from " + start + " to " + end + " is " + sum);
