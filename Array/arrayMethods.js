e9b3eed3af3c25fdeb20cd0f0395903ccc91cc23
Hasibull <hasibulislam4500@gmail.com>
2021-07-02 16:52:13 +0600

    find  method

--- Array/arrayMethods.js
+++ Array/arrayMethods.js
@@ -1,7 +1,18 @@
 var array = [10, 20, 30, 40, 50];
 
-var newArray = array.filter(function(value){
+var newArray = array.filter(function(value) {
     return value>20;
 });
 
 console.log(newArray);
+
+var element = array.find(function(value) {
+    return value==80;
+});
+
+if(element > 0) {
+    console.log('Success');
+}
+else {
+    console.log("Element doesn't exist!");
+}
