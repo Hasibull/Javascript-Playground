9a9d6b686d4890a1668cb7ba70c37c7a08e3e346
Hasibull <hasibulislam4500@gmail.com>
2021-07-02 16:44:32 +0600

    basic things about array

--- Array/arrayBasic.js
+++ Array/arrayBasic.js
@@ -1,7 +1,28 @@
 var value = [12, 15, 23, 50];
 
+console.log('main array');
 console.log(value);
 
-for(let i of value){
+console.log('before adding new element');
+for(let i of value) {
+    process.stdout.write(i + ' ');
+}
+
+console.log('');
+
+value.push(60);
+value.push(70);
+
+console.log('after adding new element');
+for(let i of value) {
+    process.stdout.write(i + ' ');
+}
+
+console.log('');
+
+value.pop();
+
+console.log('after removing a element');
+for(let i of value) {
     process.stdout.write(i + ' ');
 }
